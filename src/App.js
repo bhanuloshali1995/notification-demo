a521ca1f1ca0bbe821473e932abb9ffd5e392c1e
Bhanu Loshali <bhanuloshali@Bhanus-MacBook-Pro.local>
2021-08-11 04:09:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Notification Demo
+        Notification Demo by Bhanu
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBAqOcWyVZbLKo3Wdt4B9B7qINxEtNapeI",
+  authDomain: "netlify-demo-app.firebaseapp.com",
+  databaseURL: "https://netlify-demo-app-default-rtdb.firebaseio.com",
+  projectId: "netlify-demo-app",
+  storageBucket: "netlify-demo-app.appspot.com",
+  messagingSenderId: "738200397308",
+  appId: "1:738200397308:web:5d2df9b77449ec29a2be79",
+  measurementId: "G-BGQCY4YZPB"
+};
+firebase.initializeApp(firebaseConfig);
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,6 +26,7 @@ ReactDOM.render(
 );
 
 window.addEventListener('load', async () => {
+  const userid = await window.prompt('Enter user name');
   const registration = await navigator.serviceWorker.register('/sw.js', {
     scope: '/'
   });
@@ -21,10 +36,11 @@ window.addEventListener('load', async () => {
         userVisibleOnly: true,
         applicationServerKey: "BAA8TGQbVGRUXO-geg-vm4VBORoemXcWDEMW6MQIbwz_zL4DKAp1aaWJg2qqxhHB-xP81y0Th5i8IE88w88vGPo"
       })
-      fetch('192.168.0.1:4000/api/asd',{
-        method:'POST',
-        body: JSON.stringify(pushSubscription)
-      })
+      const db = firebase.firestore();
+      db.collection("users").add(
+        {...JSON.parse(JSON.stringify(pushSubscription)), userid }
+      ).then(()=> alert('Registered')).catch(e => {console.log(e)})
+      
     } else if (permission === 'default') {
 
     } else {
